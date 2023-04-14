1e7cca93c9e437141fe3cb21a7cfd43984963e0c
Kadu <kadu.silva2014@gmail.com>
2023-04-14 20:41:14 -0300

    🐛 fix: main.py structure
    I refactored the structure of the main.py file to follow the best practices of FastAPI and added a
    block to check for direct module execution. Now, the login check is performed inside the if __name__
    == '__main__' block, ensuring that the user is logged in before the FastAPI server is initialized.

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ from fastapi import FastAPI
 from scraping import get_Recommendations, login_linkedIn, URL_BASE
 from driver import DRIVER
 
-is_login = login_linkedIn()
+if __name__ == '__main__':
+    is_login = login_linkedIn()
 
-app = None
+    app = None
 
-if is_login:
-    app = FastAPI()
+    if is_login:
+        app = FastAPI()
 
-    @app.get("/")
-    def read_root():
-        return {"Hello": "World"}
+        @app.get("/")
+        def read_root():
+            return {"Hello": "World"}
 
-    @app.get("/username/{username}")
-    def get_recommendations(username: str):
-        recommendations = get_Recommendations(username)
-        DRIVER.get(URL_BASE)
-        return recommendations
+        @app.get("/username/{username}")
+        def get_recommendations(username: str):
+            recommendations = get_Recommendations(username)
+            DRIVER.get(URL_BASE)
+            return recommendations
