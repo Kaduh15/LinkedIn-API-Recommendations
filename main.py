23071d6b215d869bd07558275f86ad167b96bdc0
Kadu <kadu.silva2014@gmail.com>
2023-04-14 20:38:31 -0300

    ✨ feat: add main.py with FastAPI endpoints
    Created main.py with two endpoints using FastAPI. The first endpoint returns a simple "Hello World"
    message, while the second endpoint calls the function get_Recommendations to scrape LinkedIn
    recommendations for a given username. The user's recommendations are returned in JSON format. The
    login_linkedIn function is called before starting the FastAPI application to ensure that the user is
    logged in.

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+from scraping import get_Recommendations, login_linkedIn, URL_BASE
+from driver import DRIVER
+
+is_login = login_linkedIn()
+
+app = None
+
+if is_login:
+    app = FastAPI()
+
+    @app.get("/")
+    def read_root():
+        return {"Hello": "World"}
+
+    @app.get("/username/{username}")
+    def get_recommendations(username: str):
+        recommendations = get_Recommendations(username)
+        DRIVER.get(URL_BASE)
+        return recommendations
