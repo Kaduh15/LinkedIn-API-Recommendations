9840143a9b2b7f66d13fe64bd77264542f283ebf
Kadu <kadu.silva2014@gmail.com>
2023-04-14 21:47:54 -0300

    ✨ feat: add docker

--- driver.py
+++ driver.py
@@ -1,15 +1,27 @@
 import os
-from selenium import webdriver
+from selenium.webdriver import Chrome, ChromeOptions
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
 URL_BASE = "https://www.linkedin.com"
 
-opts = webdriver.ChromeOptions()
-opts.add_argument("--window-size=710,1000")
-if os.getenv('ENV') == "production":
-    opts.add_argument("--headless")
 
-DRIVER = webdriver.Chrome(options=opts)
+def set_chrome_options() -> ChromeOptions:
+    """Sets chrome options for Selenium.
+    Chrome options for headless browser is enabled.
+    """
+    chrome_options = ChromeOptions()
+    chrome_options.add_argument("--window-size=710,1000")
+    if os.getenv('ENV') == "production":
+        chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+    return chrome_options
+
+
+DRIVER = Chrome(options=set_chrome_options())
 DRIVER.get(URL_BASE)
--- main.py
+++ main.py
@@ -3,20 +3,20 @@ from fastapi import FastAPI
 from scraping import get_Recommendations, login_linkedIn, URL_BASE
 from driver import DRIVER
 
-if __name__ == '__main__':
-    is_login = login_linkedIn()
 
-    app = None
+is_login = login_linkedIn()
 
-    if is_login:
-        app = FastAPI()
+app = None
 
-        @app.get("/")
-        def read_root():
-            return {"Hello": "World"}
+if is_login:
+    app = FastAPI()
 
-        @app.get("/username/{username}")
-        def get_recommendations(username: str):
-            recommendations = get_Recommendations(username)
-            DRIVER.get(URL_BASE)
-            return recommendations
+    @app.get("/")
+    def read_root():
+        return {"Hello": "World"}
+
+    @app.get("/username/{username}")
+    def get_recommendations(username: str):
+        recommendations = get_Recommendations(username)
+        DRIVER.get(URL_BASE)
+        return recommendations
