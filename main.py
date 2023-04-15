935e30c64c7a38f61f1031faba0cfc0e4809f9f3
Kadu <kadu.silva2014@gmail.com>
2023-04-14 22:12:44 -0300

    ♻️ refactor: code in main.py
    The changes in this commit include refactoring the code in main.py to remove unused imports and
    simplify the logic for the FastAPI app. The login function has been removed as it is no longer
    necessary. The updated code includes two endpoints: the root endpoint and a recommendations endpoint
    that takes a username as a parameter and returns recommendations.

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 from fastapi import FastAPI
 
-from scraping import get_Recommendations, login_linkedIn, URL_BASE
-from driver import DRIVER
+from scraping import get_Recommendations
+
+app = FastAPI()
 
 
-is_login = login_linkedIn()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-app = None
 
-if is_login:
-    app = FastAPI()
-
-    @app.get("/")
-    def read_root():
-        return {"Hello": "World"}
-
-    @app.get("/username/{username}")
-    def get_recommendations(username: str):
-        recommendations = get_Recommendations(username)
-        DRIVER.get(URL_BASE)
-        return recommendations
+@app.get("/username/{username}")
+def get_recommendations(username: str):
+    recommendations = get_Recommendations(username)
+    return recommendations
